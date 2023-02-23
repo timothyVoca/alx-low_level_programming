0511b56a3a1697c5f3c7aff5b3aa4077bd09cca0
timothyVoca <vocalync@gmail.com>
2023-02-23 06:54:29 +0000

    Learn C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 /**
- *  *main - print 0-9 separated with commas, using putchar
- *   *Return: Always 0 (Success)
- *    */
+ * main - print 0-9 separated with commas, using putchar
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int n = '0';
+	int n = '0';
 
-			while (n <= '9')
-					{
-								putchar(n);
-										if (n != '9')
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-												n++;
-													}
-				putchar('\n');
+	while (n <= '9')
+	{
+		putchar(n);
 
-					return (0);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
 }
