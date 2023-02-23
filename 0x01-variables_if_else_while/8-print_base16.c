805c19763f5bd32924873458f60487092da24051
timothyVoca <vocalync@gmail.com>
2023-02-23 05:09:37 +0000

    Learn C

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 /**
- *  *main - prints hexadecimal base 0123456789abcdef, using putchar
- *   *Return: Always 0 (Success)
- *    */
+ * main - prints hexadecimal base 0123456789abcdef, using putchar
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int n = '0';
-			int a_to_f = 'a';
+	int n = '0';
+	int a_to_f = 'a';
 
-				while (n <= '9') /*print 0-9*/
-						{
-									putchar(n);
-											n++;
-												}
+	while (n <= '9') /*print 0-9*/
+	{
+		putchar(n);
+		n++;
+	}
 
-					while (a_to_f <= 'f') /*print a-f to finish hexbase*/
-							{
-										putchar(a_to_f);
-												a_to_f++;
-													}
+	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
+	{
+		putchar(a_to_f);
+		a_to_f++;
+	}
 
-						putchar('\n');
+	putchar('\n');
 
-							return (0);
+	return (0);
 }
