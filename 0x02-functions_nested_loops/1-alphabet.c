f07f0edc3dd0005359cd7a3b50deea86c2447578
timothyVoca <vocalync@gmail.com>
2023-03-02 17:19:22 +0000

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar functionto print
+ * 			the alphabet a - z
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
