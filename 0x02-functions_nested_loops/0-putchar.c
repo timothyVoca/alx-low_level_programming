4d8f972c0f813d99e761d2536d1312e3f9cc0a8a
timothyVoca <vocalync@gmail.com>
2023-03-02 08:46:33 +0000

    learn more

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar using putchar prototype
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar(\n);
 
 	return (0);
 }
