2b79ff8491c6af0f1d8db7205e798368465b0bac
timothyVoca <vocalync@gmail.com>
2023-03-04 02:06:46 +0000

    I give up

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
  * _strccat - function that concatenates two strings.
  *
  * @dest: pointer to destination input
- * #src: pointer to source input
+ * @src: pointer to source input
  *
  * Return: pointer to resulting string #dest
  */
