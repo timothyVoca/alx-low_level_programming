4c514633dcd226757943d0bf1a1ce5de600e98ad
timothyVoca <vocalync@gmail.com>
2023-03-04 02:32:22 +0000

    4th

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,15 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
-
 	/*find the size of dest arrays*/
 	while (dest[c])
 		c++;
 
 	/* Iterate through each src array value without the null byte*/
 	for (c2 = 0; src[c2]; c2++)
-		/*append src[c2] to dest[c] */
-		/* while overwriting the null byte in dest*/
 		dest[c++] = src[c2];
 
+	/*append src[c2] to dest[c] */
+	/* while overwriting the null byte in dest*/
 	return (dest);
 }
