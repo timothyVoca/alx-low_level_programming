1b085702d72a92f29870ea81388fd1062fb0590a
timothyVoca <vocalync@gmail.com>
2023-03-13 07:06:50 +0000

    my tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str);
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
