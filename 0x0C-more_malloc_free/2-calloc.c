dd8304490a4f2bbeb50e8715d2dd73bf0de5597b
timothyVoca <vocalync@gmail.com>
2023-03-17 13:24:21 +0000

    new changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _memset - filld memory with a constant byte
- *
- * @s: input pointer for memory
- * @b:  characters to fill
- * @n: number of bytes to be filled
- *
- * Return: a pointer to the filled memory area
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
-}
-
-/**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements
  * @size: size of bytes
@@ -34,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
@@ -42,7 +25,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	_memset(p, 0, nmemb * size)
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
 	return (p);
 }
--- 0x0C-more_malloc_free/23-calloc.c
+++ 0x0C-more_malloc_free/23-calloc.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _memset - filld memory with a constant byte
+ *
+ * @s: input pointer for memory
+ * @b:  characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: a pointer to the filled memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements
  * @size: size of bytes
@@ -15,8 +34,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 
-	unsigned int i;
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
@@ -25,8 +42,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
+	_memset(p, 0, nmemb * size)
 
-	return (0);
+	return (p);
 }
